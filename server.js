c758ba4b4440717c051eae45cab5066c90df3207
dimitermusic <dimiteryyordanov@gmail.com>
2021-10-21 19:57:30 -0700

    completed server.js file

--- server.js
+++ server.js
@@ -11,3 +11,43 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+const readFromFile = util.promisify(fs.readFile);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("/api/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => {
+    notesData = JSON.parse(data);
+    res.json(notesData);
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  const newNote = { title, text, id: uuid() };
+  notesData.push(newNote);
+  const noteString = JSON.stringify(notesData);
+  fs.writeFile(`./db/db.json`, noteString, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+  res.status(201).json(newNote);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  notesData = notesData.filter((remove) => remove.id !== id);
+  const noteString = JSON.stringify(notesData);
+  fs.writeFile(`./db/db.json`, noteString, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+  res.status(201).json(notesData);
+});
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.listen(process.env.PORT || PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
